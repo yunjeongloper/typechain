c3bebb9389ed012807607a10f8d68dee2ce27360
veronica <vero911@naver.com>
2019-09-23 16:48:35 +0900

    change project setting

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+
+const me = new Human("Veronica", 25, "female");
+
+const sayHi = (person: Human): string => {
+    return`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+}
+
+console.log(sayHi(me));
+
+export {};
